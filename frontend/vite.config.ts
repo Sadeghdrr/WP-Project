fac86851472c5e0a8bfb9c3236402d8f1b9ece2d
a.sheikhha <abolfazlsheikhha.833@gmail.com>
2026-02-27 06:43:29 +0330

    fix ssh-key issue.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["kubol.ir", "www.kubol.ir", "localhost", "127.0.0.1"],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
